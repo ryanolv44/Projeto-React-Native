4513c7def0cf79a8eee8e078680f9fd9aa5b7fea
Ryan <franciscoryan012@gmail.com>
2024-05-17 17:50:45 -0300

    add cors

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TarefaList from './components/TarefaList';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>REND</Text>
-      <StatusBar style="auto" />
+      <TarefaList />
     </View>
   );
 }
